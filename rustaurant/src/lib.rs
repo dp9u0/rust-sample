9125b8ef06825e87aa717195e5fb192efda7358a
guodp <guodp9u0@gmail.com>
2023-07-18 11:42:06 +0800

    my first mod

---
+++ rustaurant/src/lib.rs
@@ -0,0 +1,19 @@
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+        pub fn seat_at_table() {}
+    }
+    pub mod serving {
+        pub fn take_order() {}
+        pub fn serve_order() {}
+        pub fn take_payment() {}
+    }
+}
+
+fn eat_at_rustaurant() {
+    front_of_house::hosting::add_to_waitlist();
+    front_of_house::hosting::seat_at_table();
+    front_of_house::serving::take_order();
+    front_of_house::serving::serve_order();
+    front_of_house::serving::take_payment();
+}
